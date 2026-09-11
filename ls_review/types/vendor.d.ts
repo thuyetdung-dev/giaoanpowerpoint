bcfe86c9c58f630020e97fdfa253578ba011d42c
thuyetdung-dev <thuyetdung@gmail.com>
2026-09-11 23:03:23 +0700

    Delete ls_review directory

--- ls_review/types/vendor.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "mammoth/mammoth.browser" {
-  export function extractRawText(input:{arrayBuffer:ArrayBuffer}):Promise<{value:string;messages:unknown[]}>;
-}
-
-declare module "pptxgenjs/dist/pptxgen.bundle.js" {
-  const PptxGenJS: any;
-  export default PptxGenJS;
-}
